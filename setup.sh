150ce46719b429e0fa5af87c171538397b8de2f1
ServaneC <schene@student.42.fr>
2020-09-05 15:33:19 +0200

    all

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 minikube start --vm-driver=docker
 
 # start and config our metalb pod
-# kubectl apply -f srcs/metallb/metallb.yaml
+#kubectl apply -f srcs/metallb/namespacelb.yaml
 kubectl apply -f srcs/metallb/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/metallb/configlb.yaml
 
 # build our nginx image
-docker build srcs/nginx -t nginx
+docker build -t nginx ./srcs/nginx 
 
 # deploy and start nginx as a service
-kubectl apply -f srcs/nginx/nginx-deploy.yaml
 kubectl apply -f srcs/nginx/nginx-service.yaml
 
 # start the web dashboard
-# minikube dashboard
+minikube dashboard
