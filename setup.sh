6d21410a36d44a2d48759198597c9873dcbb413c
ServaneC <schene@student.42.fr>
2020-09-10 14:48:11 +0200

    all

--- setup.sh
+++ setup.sh
@@ -1,28 +1,27 @@
 # create our cluster
 minikube start --vm-driver=docker
 
+# needed in order to use locally build images
 eval $(minikube docker-env)
-# start and config our metalb pod
 
-#kubectl apply -f srcs/metallb/metallb.yaml
+# start and config our metalb pod
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl delete -f ./srcs/configlb.yaml; kubectl apply -f ./srcs/configlb.yaml
-#kubectl apply -f srcs/metallb/configlb.yaml
 
-#kubectl apply -k ./srcs/
+
+kubectl apply -k ./srcs/
 
 # build our nginx image
 docker build -t nginx_img ./srcs/nginx/
 docker build -t ftps_img ./srcs/ftps/
+docker build -t mysql_img ./srcs/mysql/
 
-# deploy and start nginx as a service
-#kubectl apply -f srcs/nginx-service.yaml
+# create and deploy our services
 kubectl delete deployments nginx; kubectl delete service nginx; kubectl create -f ./srcs/nginx-service.yaml
-
-#kubectl apply -f srcs/ftps/ftps-service.yaml
 kubectl delete deployments ftps; kubectl delete service ftps; kubectl create -f ./srcs/ftps-service.yaml
+kubectl delete deployments mysql; kubectl delete service mysql; kubectl create -f ./srcs/mysql-service.yaml
 
 # start the web dashboard
 minikube dashboard
