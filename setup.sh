ca739b13b759517b5cb709c9b8fd4758e15e5051
ServaneC <schene@student.42.fr>
2020-09-10 12:26:11 +0200

    all

--- setup.sh
+++ setup.sh
@@ -1,26 +1,24 @@
 # create our cluster
 minikube start --vm-driver=docker
 
+eval $(minikube docker-env)
 # start and config our metalb pod
 
 #kubectl apply -f srcs/metallb/metallb.yaml
-#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-#kubectl apply -f srcs/metallb/configlb.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl delete -f ./srcs/metallb/configlb.yaml; kubectl apply -f ./srcs/configlb.yaml
+kubectl delete -f ./srcs/configlb.yaml; kubectl apply -f ./srcs/configlb.yaml
 #kubectl apply -f srcs/metallb/configlb.yaml
 
-kubectl apply -k ./srcs/
-eval $(minikube docker-env)
+#kubectl apply -k ./srcs/
 
 # build our nginx image
 docker build -t nginx_img ./srcs/nginx/
 docker build -t ftps_img ./srcs/ftps/
 
 # deploy and start nginx as a service
-#kubectl apply -f srcs/nginx/nginx-service.yaml
+#kubectl apply -f srcs/nginx-service.yaml
 kubectl delete deployments nginx; kubectl delete service nginx; kubectl create -f ./srcs/nginx-service.yaml
 
 #kubectl apply -f srcs/ftps/ftps-service.yaml
--- srcs/ftps/start_ftps.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-ftps # lancer le service correctement
-rc-service ftps restart
-#rc-update add vsftpd
-# boucle infinie a refaire
-#tail -f /dev/null
