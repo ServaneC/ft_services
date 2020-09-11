eb30aa785f1751849a431c2baf0457b2788f1e7a
ServaneC <schene@student.42.fr>
2020-09-11 12:48:23 +0200

    all

--- setup.sh
+++ setup.sh
@@ -11,17 +11,25 @@ kubectl create secret generic -n metallb-system memberlist --from-literal=secret
 kubectl delete -f ./srcs/configlb.yaml; kubectl apply -f ./srcs/configlb.yaml
 
 
-kubectl apply -k ./srcs/
+#kubectl apply -k ./srcs/
 
 # build our nginx image
 docker build -t nginx_img ./srcs/nginx/
 docker build -t ftps_img ./srcs/ftps/
 docker build -t mysql_img ./srcs/mysql/
+docker build -t phpmyadmin_img ./srcs/phpmyadmin/
+docker build -t wordpress_img ./srcs/wordpress/
+docker build -t influxdb_img ./srcs/influxdb/
+docker build -t grafana_img ./srcs/grafana/
 
 # create and deploy our services
-kubectl delete deployments nginx; kubectl delete service nginx; kubectl create -f ./srcs/nginx-service.yaml
-kubectl delete deployments ftps; kubectl delete service ftps; kubectl create -f ./srcs/ftps-service.yaml
-kubectl delete deployments mysql; kubectl delete service mysql; kubectl create -f ./srcs/mysql-service.yaml
+kubectl delete deployments nginx; kubectl delete service nginx; kubectl create -f ./srcs/nginx/nginx-service.yaml
+kubectl delete deployments ftps; kubectl delete service ftps; kubectl create -f ./srcs/ftps/ftps-service.yaml
+kubectl delete deployments mysql; kubectl delete service mysql; kubectl create -f ./srcs/mysql/mysql-service.yaml
+kubectl delete deployments phpmyadmin; kubectl delete service phpmyadmin; kubectl create -f ./srcs/phpmyadmin/phpmyadmin-service.yaml
+kubectl delete deployments wordpress; kubectl delete service wordpress; kubectl create -f ./srcs/wordpress/wordpress-service.yaml
+kubectl delete deployments influxdb; kubectl delete service influxdb; kubectl create -f ./srcs/influxdb/influxdb-service.yaml
+kubectl delete deployments grafana; kubectl delete service grafana; kubectl create -f ./srcs/grafana/grafana-service.yaml
 
 # start the web dashboard
 minikube dashboard
---
+++ srcs/mysql/setup_mysql.sh
@@ -0,0 +1,8 @@
+
+
+mysql -u root -e "CREATE DATABASE my_database"
+mysql -u root -e "CREATE USER 'schene'@'localhost' IDENTIFIED WITH my_sql_pass BY '42borntocode'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON my_database.* TO 'schene'@'localhost' IDENTIFIED BY '42borntocode'"
+mysql -u root -e "FLUSH PRIVILEGES"
+
+tail -f /dev/null
