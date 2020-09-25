e1db9a8e3fcc4c1bee24d651fcef1a395539fa88
ServaneC <schene@student.42.fr>
2020-09-25 12:18:43 +0200

    pma

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
-# cleaning everything
+# cleaning everything and/or creating our cluster
 if ! kubectl version &>/dev/null; then
 	service nginx stop
-    minikube start --driver=docker
 else
     kubectl delete --all deploy
     kubectl delete --all svc
     kubectl delete --all secret
     # kubectl delete --all nodes
     kubectl delete --all pods
-    kubectl delete --all namespaces
+    # kubectl delete --all namespaces
 fi
 
-# create our cluster
-
+minikube start --driver=docker
 # needed in order to use locally build images
 eval $(minikube docker-env)
 
@@ -33,33 +31,33 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl delete -f ./srcs/config.yaml; kubectl apply -f ./srcs/config.yaml
 
-kubectl create secret generic grafana \
-	--from-literal=user="schene" \
-	--from-literal=password="schene42"
+# kubectl create secret generic grafana \
+# 	--from-literal=user="schene" \
+# 	--from-literal=password="schene42"
 
 kubectl create secret generic nginx-ssh \
 	--from-literal=user="schene" \
 	--from-literal=password="schene"
 
 # build our images
-docker build -t telegraf_img ./srcs/telegraf
+# docker build -t telegraf_img ./srcs/telegraf
 docker build -t nginx_img ./srcs/nginx
-docker build -t ftps_img ./srcs/ftps
+# docker build -t ftps_img ./srcs/ftps
 docker build -t mysql_img ./srcs/mysql
 docker build -t phpmyadmin_img ./srcs/phpmyadmin
 # docker build -t wordpress_img ./srcs/wordpress
-docker build -t influxdb_img ./srcs/influxdb
-docker build -t grafana_img ./srcs/grafana
+# docker build -t influxdb_img ./srcs/influxdb
+# docker build -t grafana_img ./srcs/grafana
 
 # create and deploy our services
-kubectl create -f ./srcs/telegraf-service.yaml
+# kubectl create -f ./srcs/telegraf-service.yaml
 kubectl apply -f ./srcs/nginx-service.yaml
-kubectl create -f ./srcs/ftps-service.yaml
+# kubectl create -f ./srcs/ftps-service.yaml
 kubectl create -f ./srcs/mysql-service.yaml
 kubectl create -f ./srcs/phpmyadmin-service.yaml
 # kubectl create -f ./srcs/wordpress-service.yaml
-kubectl create -f ./srcs/influxdb-service.yaml
-kubectl create -f ./srcs/grafana-service.yaml
+# kubectl create -f ./srcs/influxdb-service.yaml
+# kubectl create -f ./srcs/grafana-service.yaml
 
 # start the web dashboard
 minikube dashboard
--- srcs/phpmyadmin/phpmyadmin.sh
+++ srcs/phpmyadmin/phpmyadmin.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-chown -R www:www /var/lib/nginx
-chown -R www:www /www
+# chown -R www:www /var/lib/nginx
+# chown -R www:www /www
 
-/usr/sbin/nginx
 php-fpm7
+nginx
