89db010f42cef59d192f866005b69c2c4d9ea349
ServaneC <schene@student.42.fr>
2020-09-27 15:49:01 +0200

    tunnel instead of metallb

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ else
 fi
 
 minikube start --driver=docker
+minikube dashboard &
+
 # needed in order to use locally build images
 eval $(minikube docker-env)
 
@@ -21,16 +23,16 @@ eval $(minikube docker-env)
 # sed -e "s/strictARP: false/strictARP: true/" | \
 # kubectl diff -f - -n kube-system
 
-# # actually apply the changes, returns nonzero returncode on errors only
+# # # actually apply the changes, returns nonzero returncode on errors only
 # kubectl get configmap kube-proxy -n kube-system -o yaml | \
 # sed -e "s/strictARP: false/strictARP: true/" | \
 # kubectl apply -f - -n kube-system
 
-# start and config our metalb pod
+# # start and config our metalb pod
 # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 # kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# kubectl delete -f ./srcs/config.yaml; kubectl apply -f ./srcs/config.yaml
+# kubectl delete -f ./srcs/metallb-config.yaml; kubectl apply -f ./srcs/metallb-config.yaml
 
 kubectl create secret generic grafana \
 	--from-literal=user="schene" \
@@ -41,10 +43,9 @@ kubectl create secret generic grafana \
 # 	--from-literal=password="schene"
 
 # build our images
-
-# docker build -t telegraf_img ./srcs/telegraf
-# docker build -t nginx_img ./srcs/nginx
-# docker build -t ftps_img ./srcs/ftps
+docker build -t telegraf_img ./srcs/telegraf
+docker build -t nginx_img ./srcs/nginx
+docker build -t ftps_img ./srcs/ftps
 docker build -t mysql_img ./srcs/mysql
 docker build -t phpmyadmin_img ./srcs/phpmyadmin
 docker build -t wordpress_img ./srcs/wordpress
@@ -61,5 +62,4 @@ kubectl create -f ./srcs/wordpress-service.yaml
 kubectl create -f ./srcs/influxdb-service.yaml
 kubectl create -f ./srcs/grafana-service.yaml
 
-# start the web dashboard
-minikube dashboard
+echo 'user42' | minikube tunnel
