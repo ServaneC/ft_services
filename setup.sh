b42bd87da27c6441927d5f8fa93f164bad898897
ServaneC <schene@student.42.fr>
2020-09-28 18:06:06 +0200

    all

--- setup.sh
+++ setup.sh
@@ -13,17 +13,18 @@ fi
 
 #creating our cluster
 minikube start --driver=docker
+minikube dashboard &
 
 # needed in order to use locally build images
 eval $(minikube docker-env)
 
-# to enable strictARP ode with ipvs
+# # to enable strictARP ode with ipvs
 # see what changes would be made, returns nonzero returncode if different
 # kubectl get configmap kube-proxy -n kube-system -o yaml | \
 # sed -e "s/strictARP: false/strictARP: true/" | \
 # kubectl diff -f - -n kube-system
 
-# # # actually apply the changes, returns nonzero returncode on errors only
+# # actually apply the changes, returns nonzero returncode on errors only
 # kubectl get configmap kube-proxy -n kube-system -o yaml | \
 # sed -e "s/strictARP: false/strictARP: true/" | \
 # kubectl apply -f - -n kube-system
@@ -42,26 +43,24 @@ kubectl create secret generic nginx-ssh \
 	--from-literal=user="schene" \
 	--from-literal=password="schene"
 
-# build our images
-# docker build -t telegraf_img ./srcs/telegraf
-# docker build -t nginx_img ./srcs/nginx
+# # build our images
+docker build -t telegraf_img ./srcs/telegraf
+docker build -t nginx_img ./srcs/nginx
 docker build -t ftps_img ./srcs/ftps
-# docker build -t mysql_img ./srcs/mysql
-# docker build -t phpmyadmin_img ./srcs/phpmyadmin
-# docker build -t wordpress_img ./srcs/wordpress
-# docker build -t influxdb_img ./srcs/influxdb
-# docker build -t grafana_img ./srcs/grafana
+docker build -t mysql_img ./srcs/mysql
+docker build -t phpmyadmin_img ./srcs/phpmyadmin
+docker build -t wordpress_img ./srcs/wordpress
+docker build -t influxdb_img ./srcs/influxdb
+docker build -t grafana_img ./srcs/grafana
 
-# create and deploy our services
-# kubectl create -f ./srcs/telegraf-service.yaml
-# kubectl apply -f ./srcs/nginx-service.yaml
+# # create and deploy our services
+kubectl create -f ./srcs/telegraf-service.yaml
+kubectl apply -f ./srcs/nginx-service.yaml
 kubectl create -f ./srcs/ftps-service.yaml
-# kubectl create -f ./srcs/mysql-service.yaml
-# kubectl create -f ./srcs/phpmyadmin-service.yaml
-# kubectl create -f ./srcs/wordpress-service.yaml
-# kubectl create -f ./srcs/influxdb-service.yaml
-# kubectl create -f ./srcs/grafana-service.yaml
+kubectl create -f ./srcs/mysql-service.yaml
+kubectl create -f ./srcs/phpmyadmin-service.yaml
+kubectl create -f ./srcs/wordpress-service.yaml
+kubectl create -f ./srcs/influxdb-service.yaml
+kubectl create -f ./srcs/grafana-service.yaml
 
-
-minikube dashboard &
-echo 'user42' | minikube tunnel
+echo user42 | minikube tunnel
