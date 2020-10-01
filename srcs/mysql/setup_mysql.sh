575b4836dfe84e0b1f03a5301b63ed1163db2dd8
ServaneC <schene@student.42.fr>
2020-10-01 14:55:36 +0200

    all

--- srcs/mysql/setup_mysql.sh
+++ srcs/mysql/setup_mysql.sh
@@ -10,3 +10,56 @@ echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PA
 echo "FLUSH PRIVILEGES;" >> /tmp/sql
 
 /usr/bin/mysqld --console --init_file=/tmp/sql
+
+
+# # chown -R mysql:mysql /var/lib/mysql
+
+# # printf "${green}GREEN${eoc}\n"
+
+# # /usr/bin/mysqld_safe &
+
+# # printf "${blue}BLUE${eoc}\n"
+
+# sleep 5
+
+# # printf "${purple}PURPLE${eoc}\n"
+
+# mysqld --default-authentication-plugin=mysql_native_password &
+
+# sleep 5
+
+# tmpsql="/tmp/init_sql"
+# # mysqladmin -u ${DB_USER} password '${DB_PASSWORD}'
+# echo > $tmpsql \
+# "CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+# CREATE USER IF NOT EXISTS ${DB_USER} IDENTIFIED BY '${DB_PASSWORD}';
+# GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+# FLUSH PRIVILEGES;
+# GRANT ALL ON *.* TO '${DB_USER}'@'127.0.0.1' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+# GRANT ALL ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+# GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+# GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+# GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;"
+
+# if [ ! -f /var/lib/mysql/wpNewUsers ]; then
+# 	echo "done" >> /var/lib/mysql/wpNewUsers
+# 	mysql -h localhost -e "$(cat $tmpsql)"
+# 	mysql -h localhost -e "$(cat ./wordpress.sql)"
+# 	mysql -h localhost -e "$(cat ./new_users.sql)"
+# fi
+
+# rm -f $tmpsql
+
+# # SET PASSWORD FOR '${DB_USER}'@'%'=PASSWORD('${DB_PASSWORD}');
+# # select user, host FROM mysql.user;
+
+# # printf "${red}RED${eoc}\n"
+
+# # killall mysqld
+
+# # printf "${dblue}D BLUE${eoc}\n"
+
+# # printf "${green}GREEN${eoc}\n"
+
+# /usr/share/mariadb/mysql.server stop
