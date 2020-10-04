ec2e4471dbf771bc6fc5493cf9b0dc67a03302fc
ServaneC <schene@student.42.fr>
2020-10-04 18:19:56 +0200

    metallb working

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 # cleaning everything
-
 if ! kubectl version &>/dev/null; then
 	service nginx stop
 else
@@ -11,56 +10,54 @@ else
     # kubectl delete --all namespaces
 fi
 
-#creating our cluster
+#creating our cluster and starting our dashboard
 minikube start --driver=docker
 minikube dashboard &
 
 # needed in order to use locally build images
 eval $(minikube docker-env)
 
-# # to enable strictARP ode with ipvs
+# to enable strictARP ode with ipvs
 # see what changes would be made, returns nonzero returncode if different
-# kubectl get configmap kube-proxy -n kube-system -o yaml | \
-# sed -e "s/strictARP: false/strictARP: true/" | \
-# kubectl diff -f - -n kube-system
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
 
-# # actually apply the changes, returns nonzero returncode on errors only
-# kubectl get configmap kube-proxy -n kube-system -o yaml | \
-# sed -e "s/strictARP: false/strictARP: true/" | \
-# kubectl apply -f - -n kube-system
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
 
-# # start and config our metalb pod
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# kubectl delete -f ./srcs/metallb-config.yaml; kubectl apply -f ./srcs/metallb-config.yaml
+# start and config our metalb pod
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl delete -f ./srcs/metallb-config.yaml; kubectl apply -f ./srcs/metallb-config.yaml
 
-# kubectl create secret generic grafana \
-# 	--from-literal=user="schene" \
-# 	--from-literal=password="schene42"
+kubectl create secret generic grafana \
+	--from-literal=user="schene" \
+	--from-literal=password="schene42"
 
-# kubectl create secret generic nginx-ssh \
-# 	--from-literal=user="schene" \
-# 	--from-literal=password="schene"
+kubectl create secret generic nginx-ssh \
+	--from-literal=user="schene" \
+	--from-literal=password="schene"
 
 # # build our images
-# docker build -t telegraf_img ./srcs/telegraf
-# docker build -t nginx_img ./srcs/nginx
-# docker build -t ftps_img ./srcs/ftps
+docker build -t telegraf_img ./srcs/telegraf
+docker build -t nginx_img ./srcs/nginx
+docker build -t ftps_img ./srcs/ftps
 docker build -t mysql_img ./srcs/mysql
 docker build -t phpmyadmin_img ./srcs/phpmyadmin
 docker build -t wordpress_img ./srcs/wordpress
-# docker build -t influxdb_img ./srcs/influxdb
-# docker build -t grafana_img ./srcs/grafana
+docker build -t influxdb_img ./srcs/influxdb
+docker build -t grafana_img ./srcs/grafana
 
-# # # # create and deploy our services
-# kubectl create -f ./srcs/telegraf-service.yaml
-# kubectl apply -f ./srcs/nginx-service.yaml
-# kubectl create -f ./srcs/ftps-service.yaml
+# create and deploy our services
+kubectl create -f ./srcs/telegraf-service.yaml
+kubectl apply -f ./srcs/nginx-service.yaml
+kubectl create -f ./srcs/ftps-service.yaml
 kubectl create -f ./srcs/mysql-service.yaml
 kubectl create -f ./srcs/phpmyadmin-service.yaml
 kubectl create -f ./srcs/wordpress-service.yaml
-# kubectl create -f ./srcs/influxdb-service.yaml
-# kubectl create -f ./srcs/grafana-service.yaml
-
-minikube tunnel -c
+kubectl create -f ./srcs/influxdb-service.yaml
+kubectl create -f ./srcs/grafana-service.yaml
--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -1,13 +1,7 @@
 #! /bin/sh
 
-# wp-cli --allow-root user create bob bob@example.com --role=author --path=/var/www/wordpress
-
-
-# wp-cli --allow-root core install \
-#     --url=localhost:5050  \
-#     --title="WordPress ft-services"
-#     --admin_user=schene \
-#     --admin_password=schene42 \
-#     --admin_email=schene@student.42.fr
+wp core install --path=/var/www/wordpress --url=172.17.123.4:5050 --title=Wordpress_ft_Services --admin_user=schene --admin_password=pass --admin_email=schene@example.com
+# wp user create bob bob@example.com --role=author --path=/var/www/wordpress
 
 supervisord
+
