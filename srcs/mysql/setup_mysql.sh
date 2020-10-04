f268822d26fed0641034dff66b4e04f8f557dd14
ServaneC <schene@student.42.fr>
2020-10-04 15:18:29 +0200

    all

--- srcs/mysql/setup_mysql.sh
+++ srcs/mysql/setup_mysql.sh
@@ -1,10 +1,11 @@
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+mysql_install_db --user=$DB_USER --ldata=/var/lib/mysql
 
 # DB_NAME = wordpress / DB_USER = mysql / DB_PASS = pass
 
 :> /tmp/sql
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;" >> /tmp/sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> /tmp/sql
+echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> /tmp/sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> /tmp/sql
 echo "FLUSH PRIVILEGES;" >> /tmp/sql
 
 /usr/bin/mysqld --console --init_file=/tmp/sql
--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
-supervisord
+# wp-cli --allow-root user create bob bob@example.com --role=author --path=/var/www/wordpress
 
- 
+
+# wp-cli --allow-root core install \
+#     --url=localhost:5050  \
+#     --title="WordPress ft-services"
+#     --admin_user=schene \
+#     --admin_password=schene42 \
+#     --admin_email=schene@student.42.fr
+
+supervisord
