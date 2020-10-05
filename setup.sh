ab043443337588cd0bbebe2ffb2e1a1beeee4db9
ServaneC <schene@student.42.fr>
2020-10-05 11:10:00 +0200

    all

--- setup.sh
+++ setup.sh
@@ -34,30 +34,30 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl delete -f ./srcs/metallb-config.yaml; kubectl apply -f ./srcs/metallb-config.yaml
 
-kubectl create secret generic grafana \
-	--from-literal=user="schene" \
-	--from-literal=password="schene42"
+# kubectl create secret generic grafana \
+# 	--from-literal=user="schene" \
+# 	--from-literal=password="schene42"
 
-kubectl create secret generic nginx-ssh \
-	--from-literal=user="schene" \
-	--from-literal=password="schene"
+# kubectl create secret generic nginx-ssh \
+# 	--from-literal=user="schene" \
+# 	--from-literal=password="schene"
 
 # # build our images
-docker build -t telegraf_img ./srcs/telegraf
-docker build -t nginx_img ./srcs/nginx
-docker build -t ftps_img ./srcs/ftps
+# docker build -t telegraf_img ./srcs/telegraf
+# docker build -t nginx_img ./srcs/nginx
+# docker build -t ftps_img ./srcs/ftps
 docker build -t mysql_img ./srcs/mysql
 docker build -t phpmyadmin_img ./srcs/phpmyadmin
 docker build -t wordpress_img ./srcs/wordpress
-docker build -t influxdb_img ./srcs/influxdb
-docker build -t grafana_img ./srcs/grafana
+# docker build -t influxdb_img ./srcs/influxdb
+# docker build -t grafana_img ./srcs/grafana
 
 # create and deploy our services
-kubectl create -f ./srcs/telegraf-service.yaml
-kubectl apply -f ./srcs/nginx-service.yaml
-kubectl create -f ./srcs/ftps-service.yaml
+# kubectl create -f ./srcs/telegraf-service.yaml
+# kubectl apply -f ./srcs/nginx-service.yaml
+# kubectl create -f ./srcs/ftps-service.yaml
 kubectl create -f ./srcs/mysql-service.yaml
 kubectl create -f ./srcs/phpmyadmin-service.yaml
 kubectl create -f ./srcs/wordpress-service.yaml
-kubectl create -f ./srcs/influxdb-service.yaml
-kubectl create -f ./srcs/grafana-service.yaml
+# kubectl create -f ./srcs/influxdb-service.yaml
+# kubectl create -f ./srcs/grafana-service.yaml
--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-wp core install --path=/var/www/wordpress --url=172.17.123.4:5050 --title=Wordpress_ft_Services --admin_user=schene --admin_password=pass --admin_email=schene@example.com
-# wp user create bob bob@example.com --role=author --path=/var/www/wordpress
+wp core install --path=/var/www/wordpress --url=http://172.17.123.2:5050 --title=Wordpress_ft_Services --admin_user=schene --admin_password=pass --admin_email=schene@example.com
+wp user create bob bob@example.com --role=author --path=/var/www/wordpress
 
 supervisord
 
